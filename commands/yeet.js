ca9916711cb5d282c3bc2f6a0be66851a1135022
Simyon <63975668+Simyon264@users.noreply.github.com>
2020-11-01 00:27:41 +0100

    you can now yeeeettt anyone

--- commands/yeet.js
+++ commands/yeet.js
@@ -8,71 +8,69 @@ let colourDone = functions.config().messageColours.done;
 
 module.exports = {
 	name: 'yeet',
-	description: 'Yeet a user out of existence!',
+	description: 'Yeet something out of existence!',
 	category: 'fun',
-	usage: '<@user| userID>',
+	usage: '<the thing you want to yeet>',
 	perms: '',
 	cooldown: 1,
-    run: function (message, prefix, args, client) {
-		if (args.length == 2) {
-			//defining the user
-			let user = message.mentions.users.first();
-			//if no user was mentioned it will be undefined so we can just do if (user) to check if one was mentioned 
-			if (user) {
-				let id = user.username
-				let author = message.author.username
-				let yeetFile = JSON.parse(fs.readFileSync("./files/strings/yeet.json"))
-				let random = yeetFile[Math.floor(Math.random() * yeetFile.length)];
-				//4 switches for 4 parts
-				switch (random.part1) {
-					case "yeeter":
-						var part1 = `${author}`
-						break;
-					case "theYeeted":
-						var part1 = `${id}`
-						break;
-					default:
-						var part1 = random.part1
-						break;
-				}
-				switch (random.part2) {
-					case "yeeter":
-						var part2 = `${author}`
-						break;
-					case "theYeeted":
-						var part2 = `${id}`
-						break;
-					default:
-						var part2 = random.part2
-						break;
-				}
-				switch (random.part3) {
-					case "yeeter":
-						var part3 = `${author}`
-						break;
-					case "theYeeted":
-						var part3 = `${id}`
-						break;
-					default:
-						var part3 = random.part3
-						break;
-				}
-				switch (random.part4) {
-					case "yeeter":
-						var part4 = `${author}`
-						break;
-					case "theYeeted":
-						var part4 = `${id}`
-						break;
-					default:
-						var part4 = random.part4
-						break;
-				}
-				message.channel.send(`${part1}` + ` ${part2} ` + `${part3}` + ` ${part4}`)
-				//functions.embed(message.channel,"YEET",colourInfo,yeetFile[random])
+	run: function (message, prefix, args, client) {
+		if (args.length > 1) {
+			let reasonArgs = args;
+			reasonArgs.splice(0, 1);
+			let reason = reasonArgs.join(" ")
+			let id = reason
+			let author = message.author.username
+			let yeetFile = JSON.parse(fs.readFileSync("./files/strings/yeet.json"))
+			let random = yeetFile[Math.floor(Math.random() * yeetFile.length)];
+			//4 switches for 4 parts
+			switch (random.part1) {
+				case "yeeter":
+					var part1 = `${author}`
+					break;
+				case "theYeeted":
+					var part1 = `${id}`
+					break;
+				default:
+					var part1 = random.part1
+					break;
 			}
+			switch (random.part2) {
+				case "yeeter":
+					var part2 = `${author}`
+					break;
+				case "theYeeted":
+					var part2 = `${id}`
+					break;
+				default:
+					var part2 = random.part2
+					break;
+			}
+			switch (random.part3) {
+				case "yeeter":
+					var part3 = `${author}`
+					break;
+				case "theYeeted":
+					var part3 = `${id}`
+					break;
+				default:
+					var part3 = random.part3
+					break;
+			}
+			switch (random.part4) {
+				case "yeeter":
+					var part4 = `${author}`
+					break;
+				case "theYeeted":
+					var part4 = `${id}`
+					break;
+				default:
+					var part4 = random.part4
+					break;
+			}
+			message.channel.send(`${part1}` + ` ${part2} ` + `${part3}` + ` ${part4}`)
+			//functions.embed(message.channel,"YEET",colourInfo,yeetFile[random])
 		} else {
-			functions.embed(message.channel,"Error",colourWarn,"Who are you trying to yeet?")
+			functions.embed(message.channel, "Error", colourWarn, "What are you trying to yeet?")
 		}
 	}
 }
