23aaa9f505920e54988fb105747fec9970399767
Simyon <63975668+Simyon264@users.noreply.github.com>
2020-11-01 00:26:47 +0100

    Ping.js is now better

--- commands/ping.js
+++ commands/ping.js
@@ -5,39 +5,43 @@ var colourInfo = functions.config().messageColours.info;
 var colourWarn = functions.config().messageColours.warn;
 
 module.exports = {
-	name: 'ping',
-	description: 'Shuts the bot down.',
-	category: 'owner',
-	usage: '',
+    name: 'ping',
+    description: 'Gives ',
+    category: 'general',
+    usage: '',
     perms: '',
     cooldown: 10,
     run: function (message, prefix, args, client) {
+
         let days = Math.floor(client.uptime / 86400000);
         let hours = Math.floor(client.uptime / 3600000) % 24;
         let minutes = Math.floor(client.uptime / 60000) % 60;
         let seconds = Math.floor(client.uptime / 1000) % 60;
-        let ping = message.createdTimestamp - Date.now()
 
-        let colour 
-        if (ping > 500) {
+        let colour
+        if (client.ws.ping > 500) {
             colour = 0xff2121
         } else {
             colour = 0x26e08d
         }
 
-        let embed = new discord.MessageEmbed()
-            .setTitle("Pong! :ping_pong:")
-            .addField("API:", ping + "ms")
-            .addField("Client: ", client.ws.ping + "ms")
-            .addField("Uptime: ", `${days}d ${hours}h ${minutes}m ${seconds}s`)
-            .setColor(colour)
-
         message.channel.send("〽️ Pinging").then((m) => {
+            let ping = m.createdTimestamp - Date.now()
+            let embed = new discord.MessageEmbed()
+                .setTitle("Pong! :ping_pong:")
+                .addField("Bot latency: ", ping + "ms")
+                .addField("API: ", client.ws.ping + "ms")
+                .addField("Uptime: ", `${days}d ${hours}h ${minutes}m ${seconds}s`)
+                .setColor(colour)
             m.edit(embed).then((m) => {
-                m.edit("")
-            });
-        });
+                m.edit("").catch((err) => {
+                    if (err.code == "50006") {
+                        m.edit(`:rotating_light: WARNING :rotating_light:\nI need **embed links** permission!\nBot: ${ping}ms\nAPI: ${client.ws.ping}ms\nUptime: ${days}d ${hours}h ${minutes}m ${seconds}s`)
+                    }
+                })
+            })
+        }).catch((err) => {
 
-        
-	}
+        });
+    }
 }
