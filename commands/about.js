d4d07e2cdeb1a42d43aaa282d3075992429ec940
Simyon <63975668+Simyon264@users.noreply.github.com>
2020-11-01 00:26:34 +0100

    about.js now has the uptime

--- commands/about.js
+++ commands/about.js
@@ -1,4 +1,3 @@
-const config = require('../config.json');
 const functions = require('../functions.js');
 const discord = require('discord.js');
 
@@ -12,14 +11,20 @@ module.exports = {
 	usage: '',
 	perms: '',
 	cooldown: 1,
-    run: function (message, prefix,args,client) {
+	run: function (message, prefix, args, client) {
+		let days = Math.floor(client.uptime / 86400000);
+		let hours = Math.floor(client.uptime / 3600000) % 24;
+		let minutes = Math.floor(client.uptime / 60000) % 60;
+		let seconds = Math.floor(client.uptime / 1000) % 60;
+
 		let embed = new discord.MessageEmbed()
 			.setTitle("About")
 			.setColor(colourInfo)
 			.setDescription(functions.config().bot.description)
 			.addField("Version", functions.config().bot.version)
-            .addField("Author", functions.config().bot.Authors)
+			.addField("Author", functions.config().bot.Authors)
 			.setThumbnail(client.user.avatarURL())
+			.addField("Uptime", `${days}d ${hours}h ${minutes}m ${seconds}s`)
 			.addField("Invite", `[Here](${functions.config().bot.invite} 'Click me to invite me!')`)
 		message.channel.send(embed);
 	}
