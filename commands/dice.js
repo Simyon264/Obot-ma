e4dfa846affa564648312c6c8b8cd5ae03ffeb32
Simyon <63975668+Simyon264@users.noreply.github.com>
2021-03-14 13:55:28 +0100

    Added dice command

---
+++ commands/dice.js
@@ -0,0 +1,39 @@
+const functions = require('../functions.js');
+const discord = require('discord.js');
+
+var colourInfo = functions.config().messageColours.info;
+var colourWarn = functions.config().messageColours.warn;
+
+module.exports = {
+    name: 'dice',
+    description: 'Roll the dice.',
+    category: 'fun',
+    usage: 'dice [min],[max]',
+    perms: '',
+    alias: ["randomnumber", "rn"],
+    cooldown: 1,
+    run: function (message, prefix, args, client) {
+        function getRandomInt(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+        if (args.length == 2) {
+            let arg = args
+            arg.splice(0, 1);
+            const argsJoined = arg.join(' ');
+            arg = argsJoined.split(',');
+            args = arg
+            if (typeof args[1] !== 'undefined') {
+                const randomNumber = getRandomInt(args[0], args[1])
+                message.channel.send(`:game_die: ${randomNumber}`)
+            } else {
+                const randomNumber = Math.floor(Math.random() * 6)
+                message.channel.send(`:game_die: ${randomNumber}`)
+            }  
+        } else {
+            const randomNumber = Math.floor(Math.random() * 6)
+            message.channel.send(`:game_die: ${randomNumber}`)
+        }
+    }
+}
