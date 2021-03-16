a19ef1d3489fd738ea78d6999b0e7e107374c2ff
Simyon <63975668+Simyon264@users.noreply.github.com>
2021-03-16 12:30:06 +0100

    Added a newline

--- functions.js
+++ functions.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const discord = require('discord.js')
+
 exports.config = function (config) {
     try {
         return (JSON.parse(fs.readFileSync("./files/important files/config.json")))
