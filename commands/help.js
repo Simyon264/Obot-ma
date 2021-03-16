c73663f1834e102d5d5cd1130106e25194826ea2
Simyon <63975668+Simyon264@users.noreply.github.com>
2021-03-16 12:33:02 +0100

    Help now works with alias

--- commands/help.js
+++ commands/help.js
@@ -75,7 +75,53 @@ module.exports = {
 
 					message.channel.send(embed);
 				} else if (err.code === 'ENOENT') {
-					functions.embed(message.channel, "", colourWarn, "Specified command does not exist");
+					let dir = fs.readdirSync("./commands/")
+					let commandFile2
+					let found = false
+					for (let index = 0; index < dir.length; index++) {
+						commandFile2 = require(`../commands/${dir[index]}`);
+						if (commandFile2['alias'].includes(args[1].toLowerCase())) {
+							found = true;
+							var newCommandFile = index
+							index = dir.length;
+							continue;
+						}
+					}
+					if (found) {
+						let commandFile = require(`../commands/${dir[newCommandFile]}`);
+
+						var cmdName = commandFile['name'];
+						var cmdDesc = commandFile['description'];
+						var cmdCategory = commandFile['category'];
+						var cmdUsage = commandFile['usage'];
+						var cmdRoles = commandFile['perms'];
+						let cooldown = commandFile['cooldown']
+						let aliases = commandFile['alias']
+
+						let final = ""
+						for (let index = 0; index < aliases.length; index++) {
+							final = `${final}\`${aliases[index]}\`,`
+						}
+
+						cmdName = cmdName.charAt(0).toUpperCase() + cmdName.slice(1);
+						cmdCategory = cmdCategory.charAt(0).toUpperCase() + cmdCategory.slice(1);
+						cmdRoles = cmdRoles.charAt(0).toUpperCase() + cmdRoles.slice(1);
+
+						let embed = new discord.MessageEmbed()
+							.setTitle(cmdName)
+							.setColor(colourInfo)
+							.addField("Description", cmdDesc || "*none*")
+							.addField("Category", cmdCategory || "*none*")
+							.addField("Usage", cmdUsage || "*none*")
+							.addField("Permissions", cmdRoles || "*none*");
+						embed.addField("Cooldown", cooldown + " Second(s)" || "*none*")
+						embed.addField("Aliases", final || "*none*")
+						embed.setDescription(`\`[]\` means optional and \`<>\` is required. The bot prefix is \`${prefix}\``)
+						
+						message.channel.send(embed)
+					} else {
+						functions.embed(message.channel, "", colourWarn, "Specified command does not exist");
+					}
 				} else {
 					console.log(err);
 
