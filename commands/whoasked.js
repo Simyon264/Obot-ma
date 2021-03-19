a4c7515b7077f76ff0431f54bb470b6058d6f795
Simyon <63975668+Simyon264@users.noreply.github.com>
2021-03-19 09:04:46 +0100

    Changed the chances

--- commands/whoasked.js
+++ commands/whoasked.js
@@ -12,31 +12,23 @@ module.exports = {
     usage: 'whoasked',
     perms: '',
     alias: [],
-    cooldown: 2,
+    cooldown: 4,
     run: function (message, prefix, args, client) {
         let messages = []
         let randomMessage = 1
-        let randomNumber = functions.randomInt(1, 3)
-        //1-Failed 2-Random Person asked 3-Someone asked
-        switch (randomNumber) {
-            case 1:
-                messages = ["I've contacted the NSA, they're looking for who tf asked.", "nobody asked.", "Your ask will be delivered in 5-7 days", "God couldn't even find out who tf asked.", "There are like 8 billion poeple on earth and im still looking for someone who asked."]
-                randomMessage = functions.randomInt(0, messages.length)
-                message.channel.send(messages[randomMessage])
-                break;
-            case 2:
-                messages = ["this mf dumbass Mr.Pluff asked bruh.", "Spongebob asked. Not really proving anything but he asked.", "Joe asked. Who is joe? Well its JOE MAMA HAHA GOTEM"]
-                randomMessage = functions.randomInt(0, messages.length)
-                message.channel.send(messages[randomMessage])
-                break;
-            case 3:
-                messages = message.guild.members.cache.array()
-                randomMessage = functions.randomInt(0, messages.length)
-                functions.embed(message.channel, "", colourInfo, `${messages[randomMessage]} asked, allow it ig.`)
-                break;
-            default:
-                console.log(randomNumber)
-                break;
+        let randomNumber = functions.randomInt(1, 150)
+        if (randomNumber >= 0 && randomNumber <= 20) {
+            messages = message.guild.members.cache.array()
+            randomMessage = functions.randomInt(0, messages.length)
+            functions.embed(message.channel, "", colourInfo, `${messages[randomMessage]} asked, allow it ig.`)
+        } else if (randomNumber >= 21 && randomNumber <= 130) {
+            messages = ["this mf dumbass Mr.Pluff asked bruh.", "Spongebob asked. Not really proving anything but he asked.", "Joe asked. Who is joe? Well its JOE MAMA HAHA GOTEM","ur mom asked", "sorry simyon pushed the answer into lava but ig you asked",`i asked`,"what?","ur dad ask- oh right your dad has gone away for milk"]
+            randomMessage = functions.randomInt(0, messages.length)
+            message.channel.send(messages[randomMessage])
+        } else {
+            messages = ["I've contacted the NSA, they're looking for who tf asked.", "nobody asked.", "Your ask will be delivered in 5-7 days", "God couldn't even find out who tf asked.", "There are like 8 billion poeple on earth and im still looking for someone who asked."]
+            randomMessage = functions.randomInt(0, messages.length)
+            message.channel.send(messages[randomMessage])
         }
     }
 }
