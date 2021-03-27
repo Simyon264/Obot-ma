f4e498dd987ff0c8c98cd75c67dc7271087f691f
Joshua Zacek <joshuazacek@Joshuas-MacBook-Air.local>
2021-03-27 21:46:13 +0000

    Refactored bot.js code

--- bot.js
+++ bot.js
@@ -1,90 +1,70 @@
-//defining all needed things
+// Imports
 const fs = require("fs")
-const colors = require('colours')
 const discord = require("discord.js")
-const readline = require("readline")
+const colors = require('colours') // used to print custom colours in the terminal
+const readline = require("readline") // used for user to input bot token
 
 console.log("Starting bot...")
-console.log("Starting client!")
-//defining the client
-const client = new discord.Client();
-//if the token file is there it will just continue booting if not it will ask
-//creating readline interface
+const client = new discord.Client(); // discord client
+
+// Start readline interface
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-//defining a token function
+
+// Ask user for token, and store it
 function token() {
-    rl.question("Token: ", (answer) => {
-        fs.unlinkSync("./files/important files/token.txt")
+    rl.question("Enter a Discord Bot Token: ", (answer) => {
         fs.writeFileSync("./files/important files/token.txt", answer)
-        client.login(fs.readFileSync("./files/important files/token.txt", 'utf-8')).then(() => {
-            rl.close()
-        }).catch((error) => {
-            //if it errors
-            if (error.code == "TOKEN_INVALID") {
-                console.log(colors.red("Error: ") + "The token provided is invalid!")
-                token()
-            } else {
-                console.log(colors.red("Unknown error: " + error))
-            }
-        })
+        start()
     });
 }
 
-try {
-    fs.readFileSync("./files/important files/token.txt")
-    console.log("Starting files!")
-    let files = fs.readdirSync("./eventhandler/")
-    //starting all files by running through them
-    for (let index = 0; index < files.length; index++) {
-        var commandFile = require(`./eventhandler/${files[index]}`);
-        console.log(colors.yellow(`Started: ${files[index]}`));
-        commandFile['run'](client);
-    }
-    console.log(colors.green("All files started, logging in!"))
-    client.login(fs.readFileSync("./files/important files/token.txt", 'utf-8')).then(() => {
-        rl.close()
-    }).catch((error) => {
-        //if it errors
-        if (error.code == "TOKEN_INVALID") {
-            console.log(colors.red("Error: ") + "The token provided is invalid!")
-            token()
-        } else {
-            console.log(colors.red("Unknown error: " + error))
-        }
-    })
-} catch (error) {
-    console.log(colors.red("Error: ") + "token is missing!")
+// Start bot
+function start() {
+    try {
+        let token = fs.readFileSync("./files/important files/token.txt", "utf-8")
 
-    //ask the user for the token
-    rl.question("Token: ", (answer) => {
-        //write the token
-        fs.writeFileSync("./files/important files/token.txt", answer)
         console.log("Starting files!")
         let files = fs.readdirSync("./eventhandler/")
-        for (let index = 0; index < files.length; index++) {
-            var commandFile = require(`./eventhandler/${files[index]}`);
-            //commandFile['run'](client);
-            console.log(colors.yellow(`Started: ${files[index]}`));
+
+        // Start event handlers
+        for (let i = 0; i < files.length; i++) {
+            let event = require(`./eventhandler/${files[i]}`);
+            console.log(colors.yellow(`Started: ${files[i]}`));
+            event['run'](client);
         }
         console.log(colors.green("All files started, logging in!"))
-        //log into the client using the token file
-        client.login(fs.readFileSync("./files/important files/token.txt", 'utf-8')).then(() => {
-            rl.close()
-        }).catch((error) => {
-            //if it errors
-            if (error.code == "TOKEN_INVALID") {
-                console.log(colors.red("Error: ") + "The token provided is invalid!")
-                token()
-            } else {
-                console.log(colors.red("Unknown error: " + error))
-            }
-        })
-    });
+
+        try {
+            // Login using token
+            client.login(token)
+            .then(() => {
+                // Stop readline
+                rl.close()
+            })
+        } catch (err) {
+            // If the error is not a token invalid error, throw it
+            if (err.code !== "TOKEN_INVALID") throw err;
+
+            console.log(colors.red("Error: ") + "The token provided is invalid!")
+            token()
+        }
+
+    } catch (err) {
+        // If the error is not a file not found error, throw it
+        if (err.code !== "ENOENT") throw err;
+
+        console.log(colors.red("Error: ") + "token is missing!")
+        token();
+    }
 }
 
-process.on("unhandledRejection", () => {
+// Call start function
+start();
 
-});
+// NOTE: Discord.js is stupid and without that a perms error without a catch would kill the bot. Even a try catch cant stop that error. TL:DR its the last fail safe - Simon
+// This line makes me very uncomfortable - Joshua
+// I'll work hard to try and remove this - Joshua
+process.on("unhandledRejection", () => {});
