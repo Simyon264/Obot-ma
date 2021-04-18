ff52eb7ab48ed70c3df46096aec2af349c9de93b
Simyon <63975668+Simyon264@users.noreply.github.com>
2021-04-18 07:16:17 +0200

    Added execute command

---
+++ commands/execute.js
@@ -0,0 +1,43 @@
+const f = require('../functions.js');
+
+module.exports = {
+    name: 'execute',
+    description: 'Executes a command',
+    category: 'owner',
+    modcommand: true,
+    blockCMD: true,
+    usage: 'execute <command> [args]',
+    perms: '',
+    alias: ["ex"],
+    cooldown: 1,
+    run: function (message, prefix, args, client) {
+        f.log('Checking for owner...')
+        if (message.author.id == f.config().special.owner) {
+            f.log('Check passed.')
+            if (args.length >= 2) {
+                const argsN = message.content.split(" ").slice(1);
+                //if (args[1] == "execute") return message.channel.send("no");
+                message.channel.send('ok fam i gotchu')
+                let returnCode = f.execute(args[1], message, client, prefix, argsN)
+                switch (returnCode.code) {
+                    case 0:
+                        message.channel.send('yeah fam i did it')
+                        break;
+                    case 1:
+                        message.channel.send('that command is not a thing you stupid bich')
+                        break;
+                    case 2:
+                        message.channel.send('something broke lmao')
+                        f.log(`${returnCode.error}\n${returnCode.stack}\n${returnCode.error.code}`, 2, "EXECUTE > ")
+                        break;
+                    default:
+                        message.channel.send(`unknown code lmao ${returnCode.code}`)
+                        break;
+                }
+            } else message.channel.send('bruh you need to specify what command i need to execute...')
+        } else {
+            message.channel.send("yo bruh you aint my owner, im not doing this shit for you")
+            f.log('User is not owner.')
+        }
+    }
+}
